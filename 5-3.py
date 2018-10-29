68d99ec0b8f8d191ed1bcdee56623b6e68b0fe00
sudeepa14 <sudeepa.14@cse.mrt.ac.lk>
2018-10-30 05:27:53 +0530

    added filled 5

---
+++ 5-3.py
@@ -0,0 +1,9 @@
+p=1
+q=2
+while q>p:
+    p+=1
+    q+=1
+    if q%11==0 and q%12==0 and q%13==0 and q%14==0 and q%15==0 and q%16==0 and q%17==0 and q%18==0 and q%19==0 and q%20==0:
+      p=q
+      print(q)
+      break
